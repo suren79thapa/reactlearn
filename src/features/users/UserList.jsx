7f4be1cecb97c6ff7c206702307b1635187f5ed9
suren79thapa <thapasurendra447@gmail.com>
2025-07-29 21:58:55 +0545

    learnredx

--- src/features/users/UserList.jsx
+++ src/features/users/UserList.jsx
@@ -1,23 +1,21 @@
-import { Button } from "@material-tailwind/react";
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import React from "react";
+import { Button } from "@material-tailwind/react";
 import { addUser } from "./userSlice";
 
 export default function UserList() {
   const { users } = useSelector((state) => state.userSlice);
-  // the above code is asking for user information from the redux store
   const dispatch = useDispatch();
-  console.log(users);
   return (
     <div className="p-5">
       <Button
         onClick={() => dispatch(addUser(Math.floor(Math.random() * 100 + 1)))}
       >
-        Add Number
+        Add value
       </Button>
-
-      {users.map((user, i) => {
-        return <h1 key={i}>{user}</h1>;
+      {users.map((i, k) => {
+        return <h1 key={k}>{i}</h1>;
       })}
     </div>
   );
