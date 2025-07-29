8f40838ab10ae03eca3d57d2abb885385d59d99e
suren79thapa <thapasurendra447@gmail.com>
2025-07-29 16:01:52 +0545

    homechange

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,53 +1,48 @@
 import { Button, Input } from "@material-tailwind/react";
 import { Formik } from "formik";
-import { useState } from "react";
+import React, { useState } from "react";
 
-function Home() {
-  // now i will be learning about the formik here
+export default function Home() {
   const [data, setData] = useState([]);
   return (
     <div className="p-5">
       <Formik
         initialValues={{ title: "", email: "" }}
         onSubmit={(val, { resetForm }) => {
-          // console.log(" Submitted values:", val);
           setData((prev) => [...prev, val]);
           resetForm();
         }}
       >
-        {({ handleChange, handleSubmit, values }) => {
-          // console.log(" Live form values:", values);
-          return (
-            <form onSubmit={handleSubmit} className="max-w-[300px] space-y-4">
-              <div className="flex gap-4 ">
-                <Input
-                  onChange={handleChange}
-                  value={values.title}
-                  name="title"
-                  label="Title"
-                />
-                <Input
-                  onChange={handleChange}
-                  value={values.email}
-                  name="email"
-                  label="Email"
-                />
-              </div>
-              <Button type="submit">Submit</Button>
-            </form>
-          );
-        }}
+        {({ handleChange, handleSubmit, values }) => (
+          <form onSubmit={handleSubmit} className="max-w-[300px]">
+            <div className="flex gap-4">
+              <Input
+                onChange={handleChange}
+                value={values.title}
+                name="title"
+                label="Title"
+              />
+              <Input
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                label="Email"
+              />
+            </div>
+            <Button type="submit" className="mt-5">
+              submit
+            </Button>
+          </form>
+        )}
       </Formik>
       {data.map((d, i) => {
         return (
-          <h1 key={i}>
-            Name:{d.title} <br />
-            Email:{d.email}
-          </h1>
+          <div>
+            <h1>Name:{d.title}</h1>
+            <h1>Email:{d.email}</h1>
+          </div>
         );
       })}
     </div>
   );
 }
-
-export default Home;
