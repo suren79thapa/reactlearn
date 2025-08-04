99f501ee3568feabdb51b9270501fe06c5edb128
suren79thapa <thapasurendra447@gmail.com>
2025-08-04 19:57:58 +0545

    added newfeature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router";
+import { Link, NavLink } from "react-router";
 function Header() {
   return (
     <div className="bg-black text-white p-3 flex items-baseline justify-between">
-      <h1 className="text-2xl">Redux Toolkit</h1>
+      <Link to="/">
+        <h1 className="text-2xl font-bold text-blue-600 hover:underline cursor-pointer">
+          Redux Toolkit
+        </h1>
+      </Link>
       <nav className="space-x-7">
         <NavLink to={"/add-article"}>Create Article</NavLink>
         <NavLink to={"/cocktailcat"}> cocktail-category</NavLink>
