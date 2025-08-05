21b1ae02e49701be4cb9de0cf4285e8c0470d12c
suren79thapa <thapasurendra447@gmail.com>
2025-08-05 13:23:24 +0545

    deleted unnecessary

--- src/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return (
-    <div>
-      <h1 className="text-pink-200">this is about page</h1>
-    </div>
-  );
-}
--- src/local/local.js
+++
@@ -1,7 +0,0 @@
-export const setToLocal = (users) => {
-  localStorage.setItem("users", JSON.stringify(users));
-};
-export const getFromLocal = () => {
-  const users = localStorage.getItem("users");
-  return users === null ? [] : JSON.parse(users);
-};
